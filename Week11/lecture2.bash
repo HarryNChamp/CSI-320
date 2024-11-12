3ab9b6719313280562a40e75f9684a2025def750
HarryNChamp <112408779+HarryNChamp@users.noreply.github.com>
2024-11-11 20:35:59 -0500

    Add files via upload

--- Week11/lecture2.bash
+++ Week11/lecture2.bash
@@ -14,3 +14,8 @@ pagesAccessed=$(echo "$allLogs" | cut -d' ' -f3 |sort -n | uniq -c)
 getAllLogs
 pageCount
 echo "$pagesAccessed"
+
+function countingCurlAccess() {
+    file="/var/log/apache2/access.log"
+    awk '/curl/ {print $1}' /var/log/apache2/access.log | sort | uniq -c
+}
